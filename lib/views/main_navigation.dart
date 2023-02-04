90589a6a1aa5da7ce649767a2ce06692db3819b1
Reinvy <satriaoner@gmail.com>
2023-02-05 00:14:02 +0700

    init project

---
+++ lib/views/main_navigation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
